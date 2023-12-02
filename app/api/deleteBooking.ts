a96bc0f9e6830a17430ce60e32fb16ad6f9a722f
janaiscoding <jana.istrate@gmail.com>
2023-12-02 16:59:35 +0200

    Display success popup on delete

--- app/api/deleteBooking.ts
+++ app/api/deleteBooking.ts
@@ -1,10 +1,18 @@
+import { toast } from "@/components/ui/use-toast";
+
 const deleteBooking = (id: number) => {
   fetch(`/api/bookings/?id=${id}`, {
     method: "DELETE",
   })
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      if (data.message) {
+        toast({
+          title: `${data.message}`,
+          variant: "success",
+          description: `Hopefully you will book with us in the future again!`,
+        });
+      }
     })
     .catch((err) => {
       console.log(err);
