a795d146a95f331e63e9d23999408a2ec40e4099
janaiscoding <jana.istrate@gmail.com>
2023-12-06 14:21:48 +0200

    style: fix room card for mobile view

--- app/components/rooms_section/room-card.tsx
+++ app/components/rooms_section/room-card.tsx
@@ -26,7 +26,7 @@ const RoomCard = ({
         className={`max-h-[500px] ${!showDetails && "grayscale-[50%]"}`}
       />
       {showDetails && (
-        <div className="absolute bg-zinc-950/90 w-full bottom-0 py-10 px-4 text-zinc-50">
+        <div className="hidden md:block absolute bg-zinc-950/90 w-full bottom-0 py-10 px-4 text-zinc-50">
           <div className="w-full flex justify-between">
             <h2 className="text-xl">{roomName}</h2>
             <h3 className="text-orange-300">${price}/Night</h3>
@@ -37,6 +37,17 @@ const RoomCard = ({
           </button>
         </div>
       )}
+
+      <div className="md:hidden absolute bg-zinc-950/90 w-full bottom-0 py-10 px-4 text-zinc-50">
+        <div className="w-full flex justify-between">
+          <h2 className="text-xl">{roomName}</h2>
+          <h3 className="text-orange-300">${price}/Night</h3>
+        </div>
+        <button className="self-start flex items-center gap-2 text-sm hover:text-orange-300">
+          <p>View Details</p>
+          <ArrowUpRight />
+        </button>
+      </div>
     </div>
   );
 };
