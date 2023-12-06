dbf58fb57ffd9df7509f132ba4d98ec9b6558c3f
janaiscoding <jana.istrate@gmail.com>
2023-12-06 14:20:09 +0200

    docs: rewrite the work in progres paragraph

--- app/components/rooms_section/Rooms.tsx
+++ app/components/rooms_section/Rooms.tsx
@@ -8,7 +8,7 @@ import InfoPopup from "../ui/InfoPopup";
 const Rooms = () => {
   const [activeCategory, setActiveCategory] = useState<string>("single");
   const infoText =
-    "At its current stage, this section serves presentation purposes exclusively. Yet, it holds the potential for dynamic user room selection through a REST API, allowing administrators to add or remove rooms with data and images stored on a CDN. This design underscores the project's scalability for future enhancements. 😊";
+    "At its current stage, this section serves presentation purposes exclusively. Yet, it holds the potential for dynamic user room selection through a REST API, allowing administrators to add or remove rooms with data and images stored on a CDN. This design underscores the project's scalability for future enhancements, as this is a work in progress personal project. 😊";
   return (
     <section id="rooms">
       <div className="max-w-screen-2xl m-auto py-10 px-4 flex flex-col items-start gap-2">
